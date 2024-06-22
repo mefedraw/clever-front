b83d1ec6b2af2a808dfeb073662bea4be12bdd85
helloworld9999999 <123006715+helloworld9999999@users.noreply.github.com>
2024-06-22 19:17:35 +0300

    Add files via upload

--- tg_bot.py
+++ tg_bot.py
@@ -6,7 +6,7 @@ bot = telebot.TeleBot("7328939490:AAFlCpYQIIEB9EVan7TOjqpD-VTkzO6mVco")
 @bot.message_handler(commands=['start'])
 def start(message):
     markup_inline = types.InlineKeyboardMarkup()
-    web_url = types.WebAppInfo("https://helloworld9999999.github.io/s/idx_main_page.html")
+    web_url = types.WebAppInfo("https://helloworld9999999.github.io/123/idx_main_page.html")
     url_button = types.InlineKeyboardButton(text="Play the Game", web_app=web_url)  
     markup_inline.add(url_button)
     bot.send_message(
