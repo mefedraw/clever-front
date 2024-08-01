2c5432496ec676c2e2ed3040a2ec78c9bf38b908
mefedraw <fyodor.dubkov@gmail.com>
2024-08-01 12:33:19 +0300

    #

--- server.js
+++ server.js
@@ -24,6 +24,6 @@ app.get('/getFile', (req, res) => {
         .catch(error => res.status(500).json({ error: error.message }));
 });
 
-app.listen(3000, () => {
+app.listen(3001, () => {
     console.log('Server is running on port 3000');
 });
