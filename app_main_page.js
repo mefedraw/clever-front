30c1f5f950afc3a248f08144d78d3b266e9fe5db
mefedraw <fyodor.dubkov@gmail.com>
2024-08-01 11:09:34 +0300

    tg web api start

--- app_main_page.js
+++ app_main_page.js
@@ -1,12 +1,20 @@
 let tg = window.Telegram.WebApp;
 
-tg.expand()
+tg.expand();
+
 tg.ready();
 
-document.getElementById('username').innerText = tg.initDataUnsafe.user.username;
+const usernameElement = document.getElementById('username');
 
 window.addEventListener('resize', () => {
     document.body.style.height = window.innerHeight + 'px';
 });
 
 document.body.style.height = window.innerHeight + 'px';
+
+if (tg.initDataUnsafe && tg.initDataUnsafe.user) {
+    const user = tg.initDataUnsafe.user;
+    usernameElement.innerText = `@${user.username}`;
+} else {
+    usernameElement.innerText = 'user nickname error';
+}
